0a5bdc32a70d435f1e14fee275f4aba05c66c65f
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2023-09-15 19:53:41 -0400

    Update calculator.component.ts

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+  nbr1Value = 0;
+  nbr2Value = 0;
+  resultValue = 0;
 
-}
+  add(value1:string, value2: string): void {
+    this.nbr1Value = Number(value1);
+    this.nbr2Value = Number(value2);
+    this.resultValue = this.nbr1Value + this.nbr2Value;
+    }
+  sub(value1:string, value2: string): void {
+    this.nbr1Value = Number(value1);
+    this.nbr2Value = Number(value2);
+    this.resultValue = this.nbr1Value - this.nbr2Value;
+  }
+  mul(value1:string, value2: string): void {
+    this.nbr1Value = Number(value1);
+    this.nbr2Value = Number(value2);
+    this.resultValue = this.nbr1Value * this.nbr2Value;
+  }
+  div(value1:string, value2: string): void {
+    this.nbr1Value = Number(value1);
+    this.nbr2Value = Number(value2);
+    this.resultValue = this.nbr1Value / this.nbr2Value;
+  }
+  clear(): void {
+    this.nbr1Value = 0;
+    this.nbr2Value = 0;
+    this.resultValue = 0;
+  }
+  }
